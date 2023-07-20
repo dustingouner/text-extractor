74b2c5a30de43560c5d4601c7c473ddea602a0fd
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-07-20 12:21:44 -0700

    add share async and mediate library to set state for functionality to share and take photos

--- CameraScreen.js
+++ CameraScreen.js
@@ -1,43 +1,81 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
-import * as Permissions from 'expo-permissions';
+// import React, { useRef, useState, useEffect } from 'react';
+// import { View, Text, TouchableOpacity, SafeAreaView, Button, Image } from 'react-native';
+// import { Camera } from 'expo-camera';
+// import * as Permissions from 'expo-permissions';
+// import {shareAsync} from 'expo-sharing'
 
-const CameraScreen = () => {
-  const cameraRef = useRef(null);
-  const [hasPermission, setHasPermission] = useState(null);
+// const CameraScreen = () => {
+//   const cameraRef = useRef(null);
+//   const [hasPermission, setHasPermission] = useState(null);
+//   const [hasMediaLibarayPermission, setHasMediaLibraryPermission] = useState()
+//   const [photo, setPhoto] = useState()
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA);
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await Permissions.askAsync(Permissions.CAMERA);
+//       setHasPermission(status === 'granted');
+//       setHasMediaLibraryPermission(mediaLibrary.status === 'granted')
+//     })();
+//   }, []);
 
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();
-      console.log('Picture data:', photo);
-      // You can do something with the photo data, like displaying the image or uploading it.
-    }
-  };
+//   if(hasPermission === undefinied) {
+//     return <Text>Requesting permissions</Text>
+//   } else if(!cameraPermission) {
+//     return <Text>Permission for camera not granted</Text>
+//   }
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+//   const takePicture = async () => {
+//     let options = {
+//       quality:1,
+//       base64: true,
+//       exif: false,
+//     };
+//     if (cameraRef.current) {
+//       const photo = await cameraRef.current.takePictureAsync(options);
+//       console.log('Picture data:', photo);
+//       // You can do something with the photo data, like displaying the image or uploading it.
+//     }
+//     if(photo) {
+//       let sharePic = () => {
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera ref={cameraRef} style={{ flex: 1, aspectRatio: 4/3 }} type={Camera.Constants.Type.back}>
-        <TouchableOpacity onPress={takePicture} style={{ alignSelf: 'center', margin: 20 }}>
-          <Text style={{ fontSize: 20, color: 'white' }}>Take Photo</Text>
-        </TouchableOpacity>
-      </Camera>
-    </View>
-  );
-};
+//       }
+//       let savePhoto = () => {
 
-export default CameraScreen;
+//       }
+//       return (
+//       <SafeAreaView styles={styles.container}>
+//         <Image
+
+//       </SafeAreaView>
+//         )
+//     }
+//   };
+
+//   if (hasPermission === null) {
+//     return <View />;
+//   }
+//   if (hasPermission === false) {
+//     return <Text>No access to camera</Text>;
+//   }
+
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <Camera ref={cameraRef} style={{ flex: 1, aspectRatio: 4/3 }} type={Camera.Constants.Type.back}>
+//         <View style={StyleSheet.buttonContainer}></View>
+//         <TouchableOpacity onPress={takePicture} style={{ alignSelf: 'center', margin: 20 }}>
+//           <Text style={{ fontSize: 20, color: 'white' }}>Take Photo</Text>
+//         </TouchableOpacity>
+//       </Camera>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex:1,
+//     alignItems:'center',
+//     justifyContent
+//   }
+// })
+
+// export default CameraScreen;
